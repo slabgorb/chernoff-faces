5d6c430158b2b15473bdf3859989a68b0090e2bf
Keith Avery <keith.marshall.avery@gmail.com>
2013-05-02 18:51:52 -0400

    feature stubbing and comments

--- lib/chernoff-faces.rb
+++ lib/chernoff-faces.rb
@@ -3,52 +3,61 @@
 #  Creates chernoff face graphs in SVG.
 #
 module ChernoffFaces
-  class Base
-    def initialize()
-
-    end
-
-
-    def face(parameters)
-
-    end
-
-  end
 
   class Face
     ##
     # Draw a face
-    # eyes
-    # nose
-    # mouth
-    # face shape
-    # ears
-    #
+
     def initialize
 
     end
   end
 
+
+  ##
+  # Features express values on size or shape
+  # TODO: color?
+  #
   class Feature
-    def initialize(klass, value)
+    def draw
+      raise NotImplementedError('Draw should be overriden in the concrete class.')
+    end
+  end
+
+  ##
+  # smaller <-> larger
+  #
+  class Eyes < Feature
+    def draw
 
     end
   end
 
-  class Eyes < Feature
-
-  end
-
+  ##
+  # frown <-> smile
+  #
   class Mouth < Feature
+    def draw
 
+    end
   end
 
+  ##
+  # smaller <-> larger
+  #
   class Ears < Feature
+    def draw
 
+    end
   end
 
+  ##
+  # oval <-> round
+  #
   class HeadShape < Feature
+    def draw
 
+    end
   end
 
 end
