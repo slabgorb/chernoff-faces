9e4a29f4c13f41fcb3f65e0d5366d06bbb1ddc33
Keith Avery <keith.marshall.avery@gmail.com>
2013-05-02 18:45:45 -0400

    set up halfassed facial feature inheritance thing

--- lib/chernoff-faces.rb
+++ lib/chernoff-faces.rb
@@ -1,7 +1,54 @@
 
-
+##
+#  Creates chernoff face graphs in SVG.
+#
 module ChernoffFaces
   class Base
+    def initialize()
+
+    end
+
+
+    def face(parameters)
+
+    end
 
   end
+
+  class Face
+    ##
+    # Draw a face
+    # eyes
+    # nose
+    # mouth
+    # face shape
+    # ears
+    #
+    def initialize
+
+    end
+  end
+
+  class Feature
+    def initialize(klass, value)
+
+    end
+  end
+
+  class Eyes < Feature
+
+  end
+
+  class Mouth < Feature
+
+  end
+
+  class Ears < Feature
+
+  end
+
+  class HeadShape < Feature
+
+  end
+
 end
