639a01c6dc205bc56fd85965abb8a0a2ee616354
Keith Avery <keith.marshall.avery@gmail.com>
2013-05-03 21:46:36 -0400

    added reset function

--- lib/chernoff-faces/canvas.rb
+++ lib/chernoff-faces/canvas.rb
@@ -1,10 +1,7 @@
 module ChernoffFaces
   class Canvas
-    def initialize(width = 100, height = 100)
-      @doc = Nokogiri::XML.parse %Q|<?xml version="1.0" standalone="no"?>
-         <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
-         "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-           <svg xmlns="http://www.w3.org/2000/svg" version="1.1"></svg>|
+    def initialize(width = 1000, height = 1000)
+      @doc = skeleton
       @parent = @doc.at_css "svg"
       @parent['width'] = width
       @parent['height'] = height
@@ -18,6 +15,17 @@ module ChernoffFaces
       @parent['height'].to_i
     end
 
+    def reset
+      @doc = skeleton
+    end
+
+    def skeleton
+      Nokogiri::XML.parse %Q|<?xml version="1.0" standalone="no"?>
+         <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
+         "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+           <svg xmlns="http://www.w3.org/2000/svg" version="1.1"></svg>|
+    end
+
     def self.default_style(element_type = :default)
       case element_type.to_sym
         when :line then { fill:'white', stroke:'black' }
@@ -25,17 +33,23 @@ module ChernoffFaces
       end
     end
 
-    def child(node_type, attributes)
+    def child(node_type, parent, attributes, &block)
       node = Nokogiri::XML::Node.new node_type, @doc
       attributes = Canvas.default_style(node_type).merge(attributes)
       node.parent = @parent
       attributes.each_pair do |k,v|
         node[k] = v
       end
+      if block_given?
+        cache_parent = @parent
+        @parent = node
+        instance_exec(&block)
+        @parent = cache_parent
+      end
     end
 
-    def method_missing(meth, opts = { })
-      child(meth.to_s, opts)
+    def method_missing(meth, opts = { }, &block)
+      child(meth.to_s, @parent, opts, &block)
     end
 
     def to_s
